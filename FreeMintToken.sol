1e0bdbeab0358a23ab5e79c1b05301215a5e1f1e
Jay Snow <42946514+flexyledger@users.noreply.github.com>
2022-08-01 22:38:12 +0800

    Create FreeMintToken.sol

---
+++ FreeMintToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+// Made by @Flexyledger
+
+pragma solidity ^0.8.4;
+
+import "erc721a/contracts/ERC721A.sol";
+
+contract FreeMintToken is ERC721A {
+
+    uint256 public constant USER_LIMIT = 10;
+    uint256 public constant MAX_SUPPLY = 10_000;
+
+    constructor() ERC721A("FreeMintToken", "FTM") {}
+
+    function mint(uint256 quantity) external {
+        require(_totalMinted() + quantity <= MAX_SUPPLY, "Not more supply left");
+        require(_numberMinted(msg.sender) + quantity <= USER_LIMIT, "User limit reached");
+        // add allowlist verification here
+        _mint(msg.sender, quantity);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "ipfs://*****IPFS KEY***********/";
+    }
+}
